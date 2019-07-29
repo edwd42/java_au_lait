69209baaf89e8d95c544461e11712ee3a9ce6a6f
edwd42 <edwd42@yahoo.com>
2019-07-29 13:52:28 -0400

    edit ILoggable to work correctly in different Classes

--- testing123/src/main/java/net/ed/fcc/ConvertToF.java
+++ testing123/src/main/java/net/ed/fcc/ConvertToF.java
@@ -1,16 +1,18 @@
 package net.ed.fcc;
 
+import java.util.regex.Pattern;
+
+import net.ed.utils.ILoggable;
+
 // convert the fCC javascript algorithms in java
 // https://learn.freecodecamp.org/javascript-algorithms-and-data-structures/basic-algorithm-scripting/convert-celsius-to-fahrenheit
 
-public class ConvertToF {
-	
-	private double fahrenheit;
+public class ConvertToF implements ILoggable {
 	
 	public double convertToF(double celsius) {
 
-		// Celsius times 9/5, plus 32
+		// Fahrenheit = Celsius times 9/5, plus 32
+		logger.info("input: " + celsius);
 		return (celsius * 9/5) + 32;
 	}
-
 }
--- testing123/src/main/java/net/ed/utils/ILoggable.java
+++ testing123/src/main/java/net/ed/utils/ILoggable.java
@@ -3,10 +3,8 @@ package net.ed.utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import net.ed.car.CarService;
-
 public interface ILoggable {
 	
-	final Logger logger = LoggerFactory.getLogger(CarService.class);
+	final Logger logger = LoggerFactory.getLogger(ILoggable.class);
 
 }
